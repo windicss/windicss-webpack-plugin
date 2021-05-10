2f67845691155049d8430ace2036cdce02a74c54
Harlan Wilton <harlan@harlanzw.com>
2021-05-10 12:40:10 +1000

    fix: pitcher broken when postcss-loader missing

---
+++ example/nuxt/windi.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'windicss/helpers'
+
+export default defineConfig({
+  extract: {
+    include: ['**/*.{vue,js,ts}'],
+    exclude: ['node_modules', '.git', 'dist', 'mock', '.umi'],
+  },
+  attributify: true,
+  darkMode: 'class',
+  shortcuts: {
+    btn: 'rounded border border-gray-300 text-gray-600 px-4 py-2 m-2 inline-block hover:shadow',
+  },
+  theme: {
+    extend: {
+      colors: {
+        teal: {
+          100: '#453',
+          900: '#216b6b',
+        },
+      },
+    },
+  },
+})
