e4a3c5a431146a34a7824ebbe7ec7fd5d0dcb7e9
Harlan Wilton <harlan@harlanzw.com>
2021-06-15 14:09:05 +1000

    chore(pitcher): clean up code

--- src/loaders/pitcher.ts
+++ src/loaders/pitcher.ts
@@ -5,6 +5,11 @@ const postCSSLoader = (l : { path: string }) => /(\/|\\|@)postcss-loader/.test(l
 const cssLoader = (l : { path: string }) => /(\/|\\|@)css-loader/.test(l.path)
 const isPitcherLoader = (l : { ident?: string }) => `${NAME}:pitcher` === l.ident
 
+/*
+  * Move the position of the transform-template loader for Vue SFCs.
+  *
+  * We move it just after the PostCSS loader
+  */
 export const pitch = function (this: webpack.loader.LoaderContext, remainingRequest: string) {
   // remove the pitcher immediately
   const pitcherLoaderIndex = this.loaders.findIndex(isPitcherLoader)
@@ -12,29 +17,27 @@ export const pitch = function (this: webpack.loader.LoaderContext, remainingRequ
     this.loaders.splice(pitcherLoaderIndex, 1)
   }
 
-  /*
-   * Move the position of the transform-template loader for Vue SFCs.
-   *
-   * We move it just after post css
-   */
-  if (remainingRequest.indexOf('&type=style') > 0) {
-    let insertIndex = this.loaders.findIndex(postCSSLoader)
-    // just in-case they don't have post-css for whatever reason we also search for the css-loader
-    if (insertIndex === -1) {
-      insertIndex = this.loaders.findIndex(cssLoader)
-    }
-    // insert in our loader at the right spot
-    if (insertIndex !== -1) {
-      // remove all instances of the template-loader
-      let templateLoaderIndex, templateLoader
-      while ((templateLoaderIndex = this.loaders.findIndex(isTemplateLoader)) !== -1) {
-        templateLoader = this.loaders[templateLoaderIndex]
-        this.loaders.splice(templateLoaderIndex, 1)
-      }
-      // re-insert the template-loader in the right spot
-      if (templateLoader) {
-        this.loaders.splice(insertIndex + 1, 0, templateLoader)
-      }
-    }
+  // make sure we're dealing with style-loader
+  if (remainingRequest.indexOf('&type=style') === -1) {
+    return
+  }
+  let newTemplateLoaderIndex = this.loaders.findIndex(postCSSLoader)
+  // just in-case they don't have post-css for whatever reason we also search for the css-loader
+  if (newTemplateLoaderIndex === -1) {
+    newTemplateLoaderIndex = this.loaders.findIndex(cssLoader)
+  }
+  // we couldn't find either PostCSS loader or CSS loader so we bail out
+  if (newTemplateLoaderIndex === -1) {
+    return
+  }
+  // remove all instances of the template-loader
+  let templateLoaderIndex, templateLoader
+  while ((templateLoaderIndex = this.loaders.findIndex(isTemplateLoader)) !== -1) {
+    templateLoader = this.loaders[templateLoaderIndex]
+    this.loaders.splice(templateLoaderIndex, 1)
+  }
+  // re-insert the template-loader in the right spot
+  if (templateLoader) {
+    this.loaders.splice(newTemplateLoaderIndex + 1, 0, templateLoader)
   }
 }
