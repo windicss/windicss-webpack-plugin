c5a806d3ab46d0e163546ae75adf4525da4e1b36
Harlan Wilton <harlan@harlanzw.com>
2021-07-05 18:07:13 +1000

    chore(examples): make next.js example more intuitive

--- example/next/next.config.js
+++ example/next/next.config.js
@@ -1,8 +1,9 @@
-const WindiCSSWebpackPlugin = require('../../dist').default
+// const WindiCSS = require('windicss-webpack-plugin').default
+const WindiCSS = require('../../dist').default
 
 module.exports = {
   webpack: config => {
-    config.plugins.push(new WindiCSSWebpackPlugin())
+    config.plugins.push(new WindiCSS())
     return config
   },
 }
--- example/next/windi.config.ts
+++ example/next/windi.config.ts
@@ -6,7 +6,6 @@ export default defineConfig({
     exclude: ['node_modules', '.git', '.next/**/*'],
   },
   attributify: true,
-  darkMode: 'class',
   shortcuts: {
     btn: 'rounded-lg border border-gray-300 text-gray-100 bg-blue-500 px-4 py-2 m-2 inline-block hover:shadow',
   },
