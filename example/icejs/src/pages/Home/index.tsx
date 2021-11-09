4358a390167248e40c9a998be4b6f5337527daf7
wjq990112 <1163585385@qq.com>
2021-11-10 00:59:05 +0800

    chore(examples): add an example for icejs

---
+++ example/icejs/src/pages/Home/index.tsx
@@ -0,0 +1,35 @@
+const Home = () => {
+  return (
+    <div className="min-h-600px overflow-hidden text-center bg-white">
+      <h2 className="text-40px text-center">Welcome to icejs!</h2>
+      <p className="mt-40px">
+        This is an awesome project powered by&nbsp;
+        <a href="https://windicss.org" target="_blank" rel="noopener noreferrer">
+          WindiCSS
+        </a>
+        , enjoy it!
+      </p>
+      <div className="mt-40px">
+        <a
+          href="https://ice.work/docs/guide/about"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{
+            marginRight: 20,
+          }}
+        >
+          使用文档
+        </a>
+        <a href="https://github.com/ice-lab/icejs" target="_blank" rel="noopener noreferrer">
+          GitHub
+        </a>
+      </div>
+      <div className="mt-40px">
+        <div className="btn">Get Started with icejs</div>
+        <div className="btn">Get Started with WindiCSS</div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
